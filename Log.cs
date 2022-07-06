ec070ff6c4055c810ce332fd8ba285a9d391c1c2
Agerr <augustasm2006@gmail.com>
2022-07-06 23:56:39 +0300

    Added logging system

--- Log.cs
+++ Log.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Text;
 
 namespace webserver
 {
@@ -9,6 +10,7 @@ namespace webserver
         {
             logCount++;
             UpdateConsole(listener);
+            RequestLog(request);
         }
         public static void UpdateConsole(HttpListener listener)
         {
@@ -25,6 +27,22 @@ namespace webserver
             Console.WriteLine("• {0}", String.Join("\n• ", listener.Prefixes));
             Console.WriteLine(new String('-', listener.Prefixes.OrderByDescending( s => s.Length ).First().Length + 2) + Environment.NewLine);
         }
-        
+        private static void RequestLog(HttpListenerRequest request)
+        {
+            DateTime now = DateTime.Now;
+            string logPath = $"{Environment.CurrentDirectory}/Log.txt";
+
+            using (StreamWriter sw = File.AppendText(logPath))
+            {
+                sw.WriteLine($"{new String('-', 10)} {DateTime.Now.ToString("yyyy/dd/MM HH:mm:ss")} {new String('-', 10)}");
+                sw.WriteLine("Type: " + request.HttpMethod);
+                sw.WriteLine("Url: " + request.Url);
+                sw.WriteLine("IP: " + request.RemoteEndPoint);
+                sw.WriteLine("Agent: " + request.UserAgent);
+                sw.WriteLine("Endpoint: " + request.LocalEndPoint);
+                sw.WriteLine("Languages: " + String.Join('/', request.UserLanguages));
+                sw.WriteLine($"{new String('-', 22 + DateTime.Now.ToString("yyyy/dd/MM HH:mm:ss").Length)}");
+            }
+        }
     }
 }
